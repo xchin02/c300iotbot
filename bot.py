e3fe5b660c82f3aa1c1ef315127c17620995fd0f
xchin02 <83194359+xchin02@users.noreply.github.com>
2022-06-09 21:27:49 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -62,7 +62,7 @@ def main():
     # on different commands - answer in Telegram
     dp.add_handler(CommandHandler("start", start))
     dp.add_handler(CommandHandler("help", help))
-    dp.add_handler(CommandHandler("chatid", get_chat_id(update, context))
+    dp.add_handler(CommandHandler("chatid", get_chat_id)
 
     # on noncommand i.e message - echo the message on Telegram
     dp.add_handler(MessageHandler(Filters.text, echo))
