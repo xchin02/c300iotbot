53566610ba9df66d37e4968942fd7be70cbb7a87
xchin02 <83194359+xchin02@users.noreply.github.com>
2022-06-10 17:19:20 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,25 +1,18 @@
-"""
-Simple Bot to reply to Telegram messages taken from the python-telegram-bot examples.
-Deployed using heroku.
-"""
-
 import logging
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
-import os
-PORT = int(os.environ.get('PORT', 5000))
 
 # Enable logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO)
 
 logger = logging.getLogger(__name__)
-TOKEN = '5564789137:AAFUVLY-lA5iBoKqsF4WTJ0w4D_kRE_CrqY'
+TOKEN = 'YOURTELEGRAMBOTTOKEN'
 
 # Define a few command handlers. These usually take the two arguments update and
 # context. Error handlers also receive the raised TelegramError object in error.
 def start(update, context):
     """Send a message when the command /start is issued."""
-    update.message.reply_text('Welcome to C300IOTbot\nAlerts upon Raspberry Pi attacks automatically!')
+    update.message.reply_text('Welcome to C300IOTBot.\nYou can use the following commands')
 
 def help(update, context):
     """Send a message when the command /help is issued."""
@@ -54,10 +47,7 @@ def main():
     dp.add_error_handler(error)
 
     # Start the Bot
-    updater.start_webhook(listen="0.0.0.0",
-                          port=int(PORT),
-                          url_path=TOKEN)
-    updater.bot.setWebhook('https://c300iotbot.herokuapp.com/' + TOKEN)
+    updater.start_polling()
 
     # Run the bot until you press Ctrl-C or the process receives SIGINT,
     # SIGTERM or SIGABRT. This should be used most of the time, since
